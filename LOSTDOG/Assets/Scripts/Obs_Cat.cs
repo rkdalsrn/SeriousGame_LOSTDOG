ab0a4ed61fb622a21421873d50d097442d50f720
Formille <skcksgml@gmail.com>
2019-11-27 17:34:29 +0900

    1127_나찬희
    - 애니메이션 추가
    - Obs_Cat의 겹치는 부분 수정
    - 리소스의 이미지들 수정

--- LOSTDOG/Assets/Scripts/Obs_Cat.cs
+++ LOSTDOG/Assets/Scripts/Obs_Cat.cs
@@ -10,38 +10,61 @@ using UnityEngine.SceneManagement;
 public class Obs_Cat : MonoBehaviour
 {
     public GameObject dog;
+    public GameObject gamePanel;
     int isMoving;
+    int isComing;
+    float isClicked;
     public bool isfailed = false;
     public Vector3 startPoint;
+
+
+    Animator anim;
+    Animator dogAnim;
     GameObject rayScript;
-    
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        gamePanel = GameObject.Find("GamePanel");
+        isComing = 0;
+        dogAnim = dog.GetComponent<Animator>();
+    }
+
     void Update()
     {
         CatFunction();
-        InitPos();
+        SetAnim();
     }
 
     void CatFunction()
     {
         startPoint = dog.GetComponent<RayScript>().startPoint;
         isMoving = dog.GetComponent<RayScript>().isMoving;
-    }
-
-    void InitPos()
-    {
+        isClicked = gamePanel.GetComponent<GamePanel>().isClicked;
         if (isMoving == 0 && dog.transform.position == transform.position)
         {
             isfailed = true;
             print("Fail!");     //debug
-            Destroy(dog);
-            StartCoroutine(LoadSceneAfterTransition());
+            isComing = 1;
+            StartCoroutine("GameOver");
         }
         else isfailed = false;
     }
 
-    private IEnumerator LoadSceneAfterTransition()
+    IEnumerator GameOver()
     {
-        yield return new WaitForSeconds(1f);       
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex );
+        for (int n = 0; n < 6; n++)
+        {
+            dog.GetComponent<RayScript>().mingu[n].SetActive(false);
+        }
+        yield return new WaitForSeconds(1.5f);
+        dog.GetComponent<Animator>().SetFloat("isFailed", 1);
+        yield return new WaitForSeconds(2f);
+        gamePanel.GetComponent<Animator>().SetFloat("isPanelButtonClicked", 1);
+    }
+
+    void SetAnim()
+    {
+        anim.SetFloat("isComing", isComing);
     }
 }
